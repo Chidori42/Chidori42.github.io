f51ff1c5abd091487b455449eca7e9865b6697b1
Chidori42 <elfagrouch9@gmail.com>
2026-01-19 02:42:06 +0100

    .

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
-type Theme = 'dark' | 'light';
+type Theme = 'light' | 'dark';
 
 interface ThemeContextType {
   theme: Theme;
